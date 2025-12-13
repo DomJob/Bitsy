8c97812f5dcb1a1828d2a1b8dd66a3461f1dc62b
DomJob <dominic.jobin@gmail.com>
2025-12-12 22:09:27 -0500

    humble lexer REPL

--- Bitsy/Bitsy/Program.cs
+++ Bitsy/Bitsy/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bitsy.Lexing;
+using Bitsy.Reading;
+
+namespace Bitsy;
+
+public static class Program
+{
+    public static void Main()
+    {
+        while(true)
+        {
+            Console.Write(">>> ");
+            string code = Console.ReadLine() ?? "";
+
+            var lexer = new Lexer(new StringCodeReader(code));
+            while (true)
+            {
+                var token = lexer.Next();
+                Console.WriteLine($"[{token.Type}, \"{token.Literal}\"]");
+
+                if (token.Type == TokenType.End) break;
+            }
+        }
+    }
+}
